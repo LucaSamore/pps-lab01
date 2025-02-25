480939ba843c8daac924e77ed5ea863a659922a3
Luca Samorè <luca.samore.01@gmail.com>
2025-02-25 13:11:55 +0100

    test(tdd-exercise): add tests for locking the door

--- tdd-exercise/src/main/java/tdd/implementation/SmartDoorLockImpl.java
+++ tdd-exercise/src/main/java/tdd/implementation/SmartDoorLockImpl.java
@@ -4,24 +4,25 @@ import tdd.SmartDoorLock;
 
 public final class SmartDoorLockImpl implements SmartDoorLock {
 
+    private record Pin(int pin) { }
+
     private enum LockState {
         LOCKED, UNLOCKED, BLOCKED
     }
 
-    private int pin;
+    private Pin pin;
     private LockState lockState;
     private int failedAttempts = 0;
     private final int maxAttempts;
 
-    public SmartDoorLockImpl(final int pin, final int maxAttempts) {
-        this.pin = pin;
+    public SmartDoorLockImpl(final int maxAttempts) {
         this.lockState = LockState.UNLOCKED;
         this.maxAttempts = maxAttempts;
     }
 
     @Override
     public void setPin(int pin) {
-
+        this.pin = new Pin(pin);
     }
 
     @Override
@@ -31,7 +32,10 @@ public final class SmartDoorLockImpl implements SmartDoorLock {
 
     @Override
     public void lock() {
-
+        if (pinIsNotSet()) {
+            throw new IllegalStateException("PIN is not set");
+        }
+        lockState = LockState.LOCKED;
     }
 
     @Override
@@ -57,5 +61,9 @@ public final class SmartDoorLockImpl implements SmartDoorLock {
     @Override
     public void reset() {
 
+    }
+
+    private boolean pinIsNotSet() {
+        return this.pin == null;
     }
 }
--- tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
+++ tdd-exercise/src/test/java/tdd/SmartDoorLockTest.java
@@ -8,19 +8,29 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public final class SmartDoorLockTest {
 
-    private static final int DEFAULT_PIN = 1234;
     private static final int MAX_ATTEMPTS = 10;
 
     private SmartDoorLock smartDoorLock;
 
     @BeforeEach
     void beforeEach() {
-        this.smartDoorLock = new SmartDoorLockImpl(DEFAULT_PIN, MAX_ATTEMPTS);
+        this.smartDoorLock = new SmartDoorLockImpl(MAX_ATTEMPTS);
     }
 
     @Test
-    void testLockIsInitiallyUnlocked() {
-        assertFalse(smartDoorLock.isLocked());
+    void testDoorIsInitiallyUnlocked() {
+        assertFalse(this.smartDoorLock.isLocked());
     }
 
+    @Test
+    void testLockDoorWithUnsetPin() {
+        assertThrows(IllegalStateException.class, () -> this.smartDoorLock.lock());
+    }
+
+    @Test
+    void testLockDoor() {
+        this.smartDoorLock.setPin(1644);
+        this.smartDoorLock.lock();
+        assertTrue(this.smartDoorLock.isLocked());
+    }
 }
